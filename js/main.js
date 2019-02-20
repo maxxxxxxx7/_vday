aa81c7f6837e3559c5846becde39524630a647fb
max <max.graham7@gmail.com>
2019-02-20 19:21:40 +1100

    updates

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 function pic(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/01.jpg"
+var pic = "/Users/maxgraham/Desktop/_vday/assets/Morning.png"
 document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic').style.display='block';
 }
 
-function picMorn(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/fav.png"
+function picAfter(){
+var pic = "/Users/maxgraham/Desktop/_vday/assets/midMorning.png"
 document.getElementById('bigpic01').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic01').style.display='block';
 }
 
-function picAfter(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/heart.png"
+function picEve(){
+var pic = "/Users/maxgraham/Desktop/_vday/assets/afternoon.png"
 document.getElementById('bigpic02').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic02').style.display='block';
 }
+function picNight(){
+var pic = "/Users/maxgraham/Desktop/_vday/assets/evening.png"
+document.getElementById('bigpic03').src = pic.replace('90x90', '225x225');
+document.getElementById('bigpic03').style.display='block';
+}
+
 
 
 
@@ -22,4 +28,5 @@ function clearPic(){
   document.getElementById('bigpic').style.display='none';
   document.getElementById('bigpic01').style.display='none';
   document.getElementById('bigpic02').style.display='none';
+  document.getElementById('bigpic03').style.display='none';
 }
