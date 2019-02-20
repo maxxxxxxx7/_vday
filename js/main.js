b36412268ee70dabe248c04fa2080d4bb4991cb5
max <max.graham7@gmail.com>
2019-02-20 20:19:37 +1100

    te

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 function pic(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/Morning.png"
+var pic = "./assets/Morning.png"
 document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic').style.display='block';
 }
 
 function picAfter(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/midMorning.png"
+var pic = "./assets/midMorning.png"
 document.getElementById('bigpic01').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic01').style.display='block';
 }
 
 function picEve(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/afternoon.png"
+var pic = "./assets/afternoon.png"
 document.getElementById('bigpic02').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic02').style.display='block';
 }
 function picNight(){
-var pic = "/Users/maxgraham/Desktop/_vday/assets/evening.png"
+var pic = "./assets/evening.png"
 document.getElementById('bigpic03').src = pic.replace('90x90', '225x225');
 document.getElementById('bigpic03').style.display='block';
 }
